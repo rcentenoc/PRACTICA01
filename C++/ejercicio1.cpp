fa63067c620acdfc1009e8ba9a42bf63cba8c294
rcentenoc <73005760+rcentenoc@users.noreply.github.com>
2021-09-02 13:42:10 -0500

    PRACTICA 01

---
+++ C++/ejercicio1.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+char* c = "abcdef";
+int m = 11148;
+return 0;
+}
---
+++ C++/ejercicio2.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+char* c = "abcdef";
+int m = 11148;
+int x = m/8;
+return 0;
+}
